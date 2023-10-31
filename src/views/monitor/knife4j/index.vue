cb4b67f7f3b91ac7d1469ec03e0cbe64b1d59f4e
zhoushuai <zs11195310@163.com>
2023-10-31 21:33:43 +0800

    代码初始化

---
+++ src/views/monitor/knife4j/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <i-frame v-model:src="url"></i-frame>
+  </div>
+</template>
+
+<script setup lang="ts">
+const url = ref(import.meta.env.VITE_APP_KNIFE4J_ADMIN);
+</script>
