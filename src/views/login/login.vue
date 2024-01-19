547f3f570349cf3b50485ec02f3f2e145fbedde7
zhoushuai <zs11195310@163.com>
2024-01-19 20:50:01 +0800

    添加滑块验证码

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -4,45 +4,59 @@
       <h3 class="title">RuoYi-Cloud-Plus多租户管理系统</h3>
       <el-form-item prop="tenantId" v-if="tenantEnabled">
         <el-select v-model="loginForm.tenantId" filterable placeholder="请选择/输入公司名称" style="width: 100%">
-          <el-option v-for="item in tenantList" :key="item.tenantId" :label="item.companyName" :value="item.tenantId"></el-option>
-          <template #prefix><svg-icon icon-class="company" class="el-input__icon input-icon" /></template>
+          <el-option v-for="item in tenantList" :key="item.tenantId" :label="item.companyName"
+                     :value="item.tenantId"></el-option>
+          <template #prefix>
+            <svg-icon icon-class="company" class="el-input__icon input-icon"/>
+          </template>
         </el-select>
       </el-form-item>
       <el-form-item prop="username">
         <el-input v-model="loginForm.username" type="text" size="large" auto-complete="off" placeholder="账号">
-          <template #prefix><svg-icon icon-class="user" class="el-input__icon input-icon" /></template>
+          <template #prefix>
+            <svg-icon icon-class="user" class="el-input__icon input-icon"/>
+          </template>
         </el-input>
       </el-form-item>
       <el-form-item prop="password">
-        <el-input v-model="loginForm.password" type="password" size="large" auto-complete="off" placeholder="密码" @keyup.enter="handleLogin">
-          <template #prefix><svg-icon icon-class="password" class="el-input__icon input-icon" /></template>
+        <el-input v-model="loginForm.password" type="password" size="large" auto-complete="off" placeholder="密码"
+                  @keyup.enter="handleLogin">
+          <template #prefix>
+            <svg-icon icon-class="password" class="el-input__icon input-icon"/>
+          </template>
         </el-input>
       </el-form-item>
       <el-form-item prop="code" v-if="captchaEnabled">
-        <el-input v-model="loginForm.code" size="large" auto-complete="off" placeholder="验证码" style="width: 63%" @keyup.enter="handleLogin">
-          <template #prefix><svg-icon icon-class="validCode" class="el-input__icon input-icon" /></template>
+        <el-input v-model="loginForm.code" size="large" auto-complete="off" placeholder="验证码" style="width: 63%"
+                  @keyup.enter="handleLogin">
+          <template #prefix>
+            <svg-icon icon-class="validCode" class="el-input__icon input-icon"/>
+          </template>
         </el-input>
         <div class="login-code">
-          <img :src="codeUrl" @click="getCode" class="login-code-img" />
+          <img :src="codeUrl" @click="getCode" class="login-code-img"/>
         </div>
       </el-form-item>
-      <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
-      <el-form-item style="float: right;">
-        <el-button circle title="微信登录" @click="doSocialLogin('wechat')">
-          <svg-icon icon-class="wechat" />
-        </el-button>
-        <el-button circle title="MaxKey登录" @click="doSocialLogin('maxkey')">
-          <svg-icon icon-class="maxkey" />
-        </el-button>
-        <el-button circle title="Gitee登录" @click="doSocialLogin('gitee')">
-          <svg-icon icon-class="gitee" />
-        </el-button>
-        <el-button circle title="Github登录" @click="doSocialLogin('github')">
-          <svg-icon icon-class="github" />
-        </el-button>
-      </el-form-item>
+      <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 0px 0px;">记住密码</el-checkbox>
+      <el-divider>第三方登录</el-divider>
+      <div style="text-align: center">
+        <el-form-item style="display: inline-block">
+          <el-button circle title="微信登录" @click="doSocialLogin('wechat')">
+            <svg-icon icon-class="wechat"/>
+          </el-button>
+          <el-button circle title="MaxKey登录" @click="doSocialLogin('maxkey')">
+            <svg-icon icon-class="maxkey"/>
+          </el-button>
+          <el-button circle title="Gitee登录" @click="doSocialLogin('gitee')">
+            <svg-icon icon-class="gitee"/>
+          </el-button>
+          <el-button circle title="Github登录" @click="doSocialLogin('github')">
+            <svg-icon icon-class="github"/>
+          </el-button>
+        </el-form-item>
+      </div>
       <el-form-item style="width:100%;">
-        <el-button :loading="loading" size="large" type="primary" style="width:100%;" @click.prevent="handleLogin">
+        <el-button :loading="loading" size="large" type="primary" style="width:100%;" @click.prevent="loginbtn">
           <span v-if="!loading">登 录</span>
           <span v-else>登 录 中...</span>
         </el-button>
@@ -51,20 +65,24 @@
         </div>
       </el-form-item>
     </el-form>
+    <div id="captcha-div"></div>
     <!--  底部  -->
     <div class="el-login-footer">
-      <span>Copyright © 2018-2023 疯狂的狮子Li All Rights Reserved.</span>
+      <span>Copyright © 2018-2023 shuai.zhou All Rights Reserved.</span>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { getCodeImg, getTenantList } from '@/api/login';
-import { authBinding } from '@/api/system/social/auth';
-import { useUserStore } from '@/store/modules/user';
-import { LoginData, TenantVO } from '@/api/types';
-import { to } from 'await-to-js';
-import { HttpStatus } from "@/enums/RespEnum";
+import {getCodeImg, getTenantList} from '@/api/login';
+import {authBinding} from '@/api/system/social/auth';
+import {useUserStore} from '@/store/modules/user';
+import {LoginData, TenantVO} from '@/api/types';
+import {to} from 'await-to-js';
+import {HttpStatus} from "@/enums/RespEnum";
+import "@/assets/styles/captcha/css/tac.css";
+import "@/assets/styles/captcha/js/jquery.min.js";
+import "@/assets/styles/captcha/js/tac.min.js";
 
 const userStore = useUserStore();
 const router = useRouter();
@@ -79,10 +97,10 @@ const loginForm = ref<LoginData>({
 } as LoginData);
 
 const loginRules: ElFormRules = {
-  tenantId: [{ required: true, trigger: "blur", message: "请输入您的租户编号" }],
-  username: [{ required: true, trigger: 'blur', message: '请输入您的账号' }],
-  password: [{ required: true, trigger: 'blur', message: '请输入您的密码' }],
-  code: [{ required: true, trigger: 'change', message: '请输入验证码' }]
+  tenantId: [{required: true, trigger: "blur", message: "请输入您的租户编号"}],
+  username: [{required: true, trigger: 'blur', message: '请输入您的账号'}],
+  password: [{required: true, trigger: 'blur', message: '请输入您的密码'}],
+  code: [{required: true, trigger: 'change', message: '请输入验证码'}]
 };
 
 const codeUrl = ref('');
@@ -102,7 +120,25 @@ const tenantList = ref<TenantVO[]>([]);
 
 watch(() => router.currentRoute.value, (newRoute: any) => {
   redirect.value = newRoute.query && newRoute.query.redirect;
-}, { immediate: true });
+}, {immediate: true});
+
+const loginbtn = () => {
+// 样式配置
+  const config = {
+    requestCaptchaDataUrl: import.meta.env.VITE_APP_BASE_API + "/auth/slider/captcha/image",
+    validCaptchaUrl: import.meta.env.VITE_APP_BASE_API + "/auth/slider/captcha/check",
+    bindEl: "#captcha-div",
+    // 验证成功回调函数
+    validSuccess: (res: any, c: any, tac: any) => {
+      handleLogin();
+      tac.destroyWindow();
+    }
+  };
+  const style = {
+    logoUrl: "src/assets/images/logo.png"
+  };
+  new window.TAC(config, style).init();
+};
 
 const handleLogin = () => {
   loginRef.value?.validate(async (valid: boolean, fields: any) => {
@@ -124,7 +160,7 @@ const handleLogin = () => {
       // 调用action的登录方法
       const [err] = await to(userStore.login(loginForm.value));
       if (!err) {
-        await router.push({ path: redirect.value || '/' });
+        await router.push({path: redirect.value || '/'});
         loading.value = false;
       } else {
         loading.value = false;
@@ -144,7 +180,7 @@ const handleLogin = () => {
  */
 const getCode = async () => {
   const res = await getCodeImg();
-  const { data } = res;
+  const {data} = res;
   captchaEnabled.value = data.captchaEnabled === undefined ? true : data.captchaEnabled;
   if (captchaEnabled.value) {
     codeUrl.value = 'data:image/gif;base64,' + data.img;
@@ -170,7 +206,7 @@ const getLoginData = () => {
  * 获取租户列表
  */
 const initTenantList = async () => {
-  const { data } = await getTenantList();
+  const {data} = await getTenantList();
   tenantEnabled.value = data.tenantEnabled === undefined ? true : data.tenantEnabled;
   if (tenantEnabled.value) {
     tenantList.value = data.voList;
@@ -199,8 +235,6 @@ const doSocialLogin = (type: string) => {
     }
   });
 };
-
-
 
 onMounted(() => {
   getCode();
@@ -279,5 +313,12 @@ onMounted(() => {
 .login-code-img {
   height: 40px;
   padding-left: 12px;
+}
+
+#captcha-div {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
