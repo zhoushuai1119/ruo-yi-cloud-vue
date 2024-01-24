c0ed03060d57e4616c6d80b20938d3060fd84b34
zhoushuai <zs11195310@163.com>
2024-01-24 22:36:47 +0800

    登录页背景图片修改

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -251,7 +251,7 @@ onMounted(() => {
   justify-content: center;
   align-items: center;
   height: 100%;
-  background-image: url("../../assets/images/login-background.jpg");
+  background-image: url("@/assets/images/login-background.jpg");
   background-size: cover;
 }
 
