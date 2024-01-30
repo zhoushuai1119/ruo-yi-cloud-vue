580532405d7a07c7a317f5846017e82bc5f2ebb2
zhoushuai <zs11195310@163.com>
2024-01-30 22:11:25 +0800

    显示密码框

--- src/api/system/sms/index.ts
+++ src/api/system/sms/index.ts
@@ -1,9 +1,7 @@
 import request from '@/utils/request';
-import { AxiosPromise } from 'axios';
-import { SmsCodeResult } from '@/api/system/sms/types';
 
 // 发送短信验证码
-export function smsCode(phonenumber: string): AxiosPromise<SmsCodeResult> {
+export function smsCode(phonenumber: string) {
   return request({
     url: '/resource/sms/code/' + phonenumber,
     method: 'get'
--- src/api/system/sms/types.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 发送短信验证码返回
- */
-export interface SmsCodeResult {
-  success: boolean;
-  data?: any;
-  configId?: string;
-}
